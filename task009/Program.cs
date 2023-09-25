f1c894c3e2371fdf009e2549f8628e0b6a03eec7
Kate <kate.klimenkova@mail.ru>
2023-09-25 15:29:29 +0300

    sem2part

---
+++ task009/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+int number = new Random().Next(10, 100); //[10,100)
+Console.WriteLine($"случайное число: {number}");
+int digitFirst = number/10; // 45%10=5, а когда мы делаем 45/10=4.5=4(и 5 остается в остатке)
+int digitSecond = number%10;
+if(digitFirst > digitSecond)
+{
+    Console.WriteLine(digitFirst);
+}
+else
+   { 
+    if(digitSecond > digitFirst)
+{
+    Console.WriteLine(digitSecond);
+}
+    else
+    {
+        Console.WriteLine("цифры равны");
+    }
+   }
---
+++ task011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int N = new Random().Next(100,1000);
+Console.WriteLine($"случайное число: {N}");
+int digitFirst = N/100;
+int digitThird = N%10;
+Console.Write(digitFirst);
+Console.Write(digitThird);
