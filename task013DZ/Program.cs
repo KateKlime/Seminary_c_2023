b9b6aa42584e8ac4437052a86b91ac08e85ab48b
KateKlime <kate_klimenkova@mail.ru>
2023-09-26 23:21:35 +0300

    fifth commit

--- task013DZ/Program.cs
+++ task013DZ/Program.cs
@@ -4,12 +4,23 @@
 //32679 -> 6
 
 Console.WriteLine("Введите число");
-int Num = Convert.ToInt32(Console.ReadLine);
-if(Num < 100 )
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num < 100)
 {
     Console.WriteLine("третьей цифры нет");
 }
 else
 {
-    
+    if (Num > 999)
+    {
+        while(Num > 999)
+        {
+            Num = Num / 10;
+        }
+    }
+    else
+    {
+        int thirdnum = Num % 10;
+        Console.WriteLine(thirdnum);
+    }
 }
